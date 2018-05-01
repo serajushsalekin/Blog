5d0c06b58261bc9caa78a8093b6f793c64ad8c1b
serajushsalekin <serajushsalekin@gmail.com>
2018-05-01 11:07:44 +0600

    title added

--- blogapp/views.py
+++ blogapp/views.py
@@ -1,6 +1,126 @@
-from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.shortcuts import render, redirect, get_object_or_404
+from blogapp.models import Post, Comments
+from django.contrib.auth.decorators import login_required
+from django.utils import timezone
+from blogapp.forms import PostForm,CommentForm
+from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import TemplateView, ListView, DetailView, CreateView, UpdateView, DeleteView
 
 
 class About(TemplateView):
     template_name = 'about.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['name'] = 'About'
+        return context
+
+
+class PostListView(ListView):
+    model = Post
+
+    def get_queryset(self):
+        return Post.objects.filter(publish_date__lte=timezone.now()).order_by('-publish_date')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['name'] = 'Post list'
+        return context
+
+
+class PostDetailView(DetailView):
+    model = Post
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['name'] = self.object.title
+        return context
+
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    login_url = '/login/'
+    redirect_field_name = 'blogapp/post_detail.html'
+    form_class = PostForm
+    model = Post
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['name'] = 'Create Post'
+        return context
+
+
+class UpdatePost(LoginRequiredMixin,UpdateView):
+    login_url = '/login/'
+    redirect_field_name = 'blogapp/post_detail.html'
+    form_class = PostForm
+    model = Post
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['name'] = ' Update Post'
+        return context
+
+
+class PostDelete(LoginRequiredMixin, DeleteView):
+    model = Post
+    success_url = reverse_lazy('post_list')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['name'] = 'Delete page'
+        return context
+
+
+class DraftView(LoginRequiredMixin, ListView):
+    login_url = '/login/'
+    redirect_field_name = 'blogapp/post_list.html'
+    model = Post
+
+    def get_queryset(self):
+        return Post.objects.filter(publish_date__isnull=True).order_by('create_date')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['name'] = 'Draft Post'
+        return context
+
+
+@login_required
+def create_comment(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = CommentForm()
+    name = "Create comment"
+    return render(request, 'blogapp/comment_form.html', {'form': form, 'name': name})
+
+
+@login_required
+def comment_approve(request, pk):
+    comment = get_object_or_404(Comments, pk=pk)
+    comment.approve()
+    redirect('post_detail', pk=Comments.post.pk)
+
+
+@login_required
+def delete_comment(request, pk):
+    comment = get_object_or_404(Comments, pk=pk)
+    post_pk = Comments.post.pk
+    comment.delete()
+    redirect('post_detail', pk=post_pk)
+
+
+@login_required
+def post_publish(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.publish()
+    redirect('post_detail', pk=pk)
+
+
